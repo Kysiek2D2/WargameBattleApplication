e29ea7d143ce083365eb72715dc1fc5d34490698
Kysiek2D2 <krz.dabkowski@gmail.com>
2023-12-09 22:05:00 +0100

    Battleground added and centered.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WargameBattleApplication
+
+* How to start server on local?
+- Open terminal in server.js location and execute 'npm start'. 
+- Then enter site like http://localhost:3000
+- To stop server, in terminal do 'ctr+C' shortcut.
+- Any changes in application will be immadiatelly visible on page from server due to nodemon library.
+
+*  
--- public/index.js
+++ public/index.js
@@ -1,15 +1,24 @@
+BATTLEGROUND_SCREEN_WIDTH_PRC = 0.8;
+BATTLEGROUND_SCREEN_HEIGHT_PRC = 0.53;
+
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth*BATTLEGROUND_SCREEN_WIDTH_PRC,
+    height: window.innerHeight*BATTLEGROUND_SCREEN_HEIGHT_PRC,
     scene: {
         preload: preload,
         create: create,
         update: update
+    },
+    scale: {
+        mode: Phaser.Scale.CENTER_BOTH,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
     }
 };
 
 var game = new Phaser.Game(config);
+var battleground;
+var sidePanel;
 
 function preload() {
     //Example of loading asset with managing cache. Cache is bypassed by adding query parameter to it.
@@ -22,13 +31,22 @@ function preload() {
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
+    
+    this.load.image({key: 'universalGrassBattleground', url: 'assets/battlegrounds72x48/universalGrass.jpg', cache: { crossOrigin: 'anonymous', responseType: 'blob', noCache: true }});
 
 }
 
 function create() {
-    this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'star');
+    
+    // this.add.image(BATTLEGROUND_WIDTH/2, BATTLEGROUND_HEIGHT*2, 'universalGrass');
+    // this.add.image(400, 300, 'star')
+    battleground = this.add.image(config.width/2, config.height, 'universalGrassBattleground');
+    battleground.width = window.innerWidth*BATTLEGROUND_SCREEN_WIDTH_PRC;
+    battleground.height = window.innerHeight*BATTLEGROUND_SCREEN_HEIGHT_PRC;
 
+    sidePanel = this.add.image(config.width/2, config.height, 'white');
+    sidePanel.width = window.innerWidth * 0.1;
+    sidePanel.height = battleground.height;
 }
 
 function update() {
