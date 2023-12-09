b50ee41f81ab4349949a3d4ef8d6caa465acbf8c
Kysiek2D2 <krz.dabkowski@gmail.com>
2023-12-09 22:42:54 +0100

    Images rotated, better sizes and centered battleground.

--- public/index.js
+++ public/index.js
@@ -3,8 +3,8 @@ BATTLEGROUND_SCREEN_HEIGHT_PRC = 0.53;
 
 var config = {
     type: Phaser.AUTO,
-    width: window.innerWidth*BATTLEGROUND_SCREEN_WIDTH_PRC,
-    height: window.innerHeight*BATTLEGROUND_SCREEN_HEIGHT_PRC,
+    width: window.innerWidth*0.95,
+    height: window.innerHeight*0.8,
     scene: {
         preload: preload,
         create: create,
@@ -21,32 +21,16 @@ var battleground;
 var sidePanel;
 
 function preload() {
-    //Example of loading asset with managing cache. Cache is bypassed by adding query parameter to it.
-    this.load.image({key: 'sky', url: 'assets/sky.png', cache: { crossOrigin: 'anonymous', responseType: 'blob', noCache: true }});
-    //
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude',
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
-    
-    this.load.image({key: 'universalGrassBattleground', url: 'assets/battlegrounds72x48/universalGrass.jpg', cache: { crossOrigin: 'anonymous', responseType: 'blob', noCache: true }});
 
+    this.load.image({key: 'universalGrassBattleground', url: 'assets/battlegrounds72x48/universalGrass.jpg'});
 }
 
 function create() {
-    
-    // this.add.image(BATTLEGROUND_WIDTH/2, BATTLEGROUND_HEIGHT*2, 'universalGrass');
-    // this.add.image(400, 300, 'star')
-    battleground = this.add.image(config.width/2, config.height, 'universalGrassBattleground');
-    battleground.width = window.innerWidth*BATTLEGROUND_SCREEN_WIDTH_PRC;
-    battleground.height = window.innerHeight*BATTLEGROUND_SCREEN_HEIGHT_PRC;
-
-    sidePanel = this.add.image(config.width/2, config.height, 'white');
-    sidePanel.width = window.innerWidth * 0.1;
-    sidePanel.height = battleground.height;
+    battleground = this.add.image(config.width/2, config.height/2, 'universalGrassBattleground');
+    battleground.setOrigin(0.5, 0.5);
+    battleground.setPosition(config.width/2, config.height/2);
+    battleground.width = config.width*BATTLEGROUND_SCREEN_WIDTH_PRC;
+    battleground.height = config.height*BATTLEGROUND_SCREEN_HEIGHT_PRC;
 }
 
 function update() {
