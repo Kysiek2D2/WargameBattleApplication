0d8ba6d1f18fbd6ee8deca9c7af4b86c38cf51e7
Kysiek2D2 <krz.dabkowski@gmail.com>
2023-12-10 13:53:06 +0100

    Extensions, snippets and VS configuration added.

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-BATTLEGROUND_SCREEN_WIDTH_PRC = 0.8;
-BATTLEGROUND_SCREEN_HEIGHT_PRC = 0.53;
+var BATTLEGROUND_SCREEN_WIDTH_PRC = 0.8;
+var BATTLEGROUND_SCREEN_HEIGHT_PRC = 0.53;
 
 var config = {
     type: Phaser.AUTO,
-    width: window.innerWidth*0.95,
-    height: window.innerHeight*0.8,
+    width: window.innerWidth * 0.95,
+    height: window.innerHeight * 0.8,
     scene: {
         preload: preload,
         create: create,
@@ -23,26 +23,27 @@ var config = {
 var game = new Phaser.Game(config);
 var battleground;
 var sidePanel;
+var unit;
 
 function preload() {
 
-    this.load.image({key: 'universalGrassBattleground', url: 'assets/battlegrounds72x48/universalGrass.jpg'});
-    this.load.image({key: 'basicInfantryUnitSizeL', url: 'assets/units/basic-infantry-size-L.png'});
+    this.load.image({ key: 'universalGrassBattleground', url: 'assets/battlegrounds72x48/universalGrass.jpg' });
+    this.load.image({ key: 'basicInfantryUnitSizeL', url: 'assets/units/basic-infantry-size-L.png' });
 }
 
 function create() {
-    battleground = this.add.image(config.width/2, config.height/2, 'universalGrassBattleground');
+    battleground = this.add.image(config.width / 2, config.height / 2, 'universalGrassBattleground');
     battleground.setOrigin(0.5, 0.5);
-    battleground.setPosition(config.width/2, config.height/2);
-    battleground.width = config.width*BATTLEGROUND_SCREEN_WIDTH_PRC;
-    battleground.height = config.height*BATTLEGROUND_SCREEN_HEIGHT_PRC;
+    battleground.setPosition(config.width / 2, config.height / 2);
+    battleground.width = config.width * BATTLEGROUND_SCREEN_WIDTH_PRC;
+    battleground.height = config.height * BATTLEGROUND_SCREEN_HEIGHT_PRC;
 
     unit = this.add.image(1100, 110, 'basicInfantryUnitSizeL')
-    .setOrigin(0.5, 0.5)
-    .setScale(0.1, 0.1)
-    .setInteractive({draggable: true});
+        .setOrigin(0.5, 0.5)
+        .setScale(0.1, 0.1)
+        .setInteractive({ draggable: true });
     this.input.setDraggable(unit);
-    this.input.on('drag', function(pointer, gameObject, dragX, dragY) {
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
         gameObject.x = dragX;
         gameObject.y = dragY;
     })
