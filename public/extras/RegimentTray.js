44228cd2c794b0a1bb804ff0b20180d28f4af7cd
K2D2 <krz.dabkowski@gmail.com>
2024-06-09 22:25:06 +0200

    RegimentTray finished

--- public/extras/RegimentTray.js
+++ public/extras/RegimentTray.js
@@ -13,43 +13,70 @@ class RegimentTray {
         this.height = regiment.height * this.proportion;
         this.color = regiment.color;
         this.isVisible = isVisible;
+        this.transparentParameter = transparentParameter;
 
-        this.trayRectangle = this.scene.add.rectangle(this.x, this.y, this.width, this.height, CONSTANTS.BASIC_COLORS.BLACK);
-        this.container.add(this.trayRectangle);
-        this.trayRectangle.setAlpha(transparentParameter);
-        this.trayRectangle.setStrokeStyle(1, CONSTANTS.BASIC_COLORS.BLACK);
+        this.createTrayRectangle();
 
-        this.triangle = this.scene.add.triangle((this.x + this.width / 2) - 1 / this.proportion, this.y, 0, 0, 0, 2, 2, 1, CONSTANTS.BASIC_COLORS.SNOW_WHITE);
-        this.triangle.setScale(1, this.proportion * 2);
-        this.triangle.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
-        this.container.add(this.triangle);
+        this.createTriangleRight();
 
-        this.triangle2 = this.scene.add.triangle((this.x - this.width / 2) + 1 / this.proportion, this.y, 2, 1, 0, 2, 0, 0, CONSTANTS.BASIC_COLORS.SNOW_WHITE);
-        this.triangle2.setScale(1, this.proportion * 2);
-        this.triangle2.setRotation(Math.PI);
-        this.triangle2.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
-        this.container.add(this.triangle2);
+        this.createTriangleLeft();
 
-        this.triangle4 = this.scene.add.triangle(this.x, (this.y + this.height / 2) - 1 / this.proportion, 0, 2, 2, 2, 1, 0, CONSTANTS.BASIC_COLORS.SNOW_WHITE);
-        this.triangle4.setScale(this.proportion * 2, 1);
-        this.triangle4.setRotation(Math.PI);
-        this.triangle4.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
-        this.container.add(this.triangle4);
+        this.createTriangleBack();
 
-        this.triangle3 = this.scene.add.triangle(this.x, (this.y - this.height / 2) + 1 / this.proportion, 0, 2, 2, 2, 1, 0, CONSTANTS.BASIC_COLORS.SNOW_WHITE);
-        this.triangle3.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
-        this.triangle3.setScale(this.proportion * 2, 1);
-        this.triangle3.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
-        this.container.add(this.triangle3);
+        this.createTriangleFront();
 
-        this.frontLine = this.scene.add.rectangle(this.x, ((this.y - this.height / 2) + 1 / this.proportion), 2, this.width, this.color);
-        this.frontLine.setRotation(Math.PI / 2);
-        this.container.add(this.frontLine);
+        this.createTriangleFacing(transparentParameter);
 
-        this.container.bringToTop(this.triangle3);
-        this.container.sendToBack(this.trayRectangle);
+        this.container.bringToTop(this.triangleFront);
 
         this.setVisibility(this.isVisible);
+    }
+
+    createTriangleFacing(transparentParameter) {
+        this.triangleFacing = this.scene.add.triangle(this.x, (this.y - this.height / 2), 0, 0, 1, Math.sqrt(50), 2, 0, this.color);
+        this.triangleFacing.setOrigin(0.5, 0);
+        this.triangleFacing.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
+        this.triangleFacing.setRotation(Math.PI);
+        this.triangleFacing.setScale(this.proportion * 2, 1);
+        this.triangleFacing.setAlpha(transparentParameter);
+        this.container.add(this.triangleFacing);
+    }
+
+    createTriangleFront() {
+        this.triangleFront = this.scene.add.triangle(this.x, (this.y - this.height / 2) + 1 / this.proportion, 0, 2, 2, 2, 1, 0, CONSTANTS.BASIC_COLORS.SNOW_WHITE);
+        this.triangleFront.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
+        this.triangleFront.setScale(this.proportion * 2, 1);
+        this.container.add(this.triangleFront);
+    }
+
+    createTriangleBack() {
+        this.triangleBack = this.scene.add.triangle(this.x, (this.y + this.height / 2) - 1 / this.proportion, 0, 2, 2, 2, 1, 0, CONSTANTS.BASIC_COLORS.SNOW_WHITE);
+        this.triangleBack.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
+        this.triangleBack.setScale(this.proportion * 2, 1);
+        this.triangleBack.setRotation(Math.PI);
+        this.container.add(this.triangleBack);
+    }
+
+    createTriangleLeft() {
+        this.triangleLeft = this.scene.add.triangle((this.x - this.width / 2) + 1 / this.proportion, this.y, 2, 1, 0, 2, 0, 0, CONSTANTS.BASIC_COLORS.SNOW_WHITE);
+        this.triangleLeft.setScale(1, this.proportion * 2);
+        this.triangleLeft.setRotation(Math.PI);
+        this.triangleLeft.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
+        this.container.add(this.triangleLeft);
+    }
+
+    createTriangleRight() {
+        this.triangleRight = this.scene.add.triangle((this.x + this.width / 2) - 1 / this.proportion, this.y, 0, 0, 0, 2, 2, 1, CONSTANTS.BASIC_COLORS.SNOW_WHITE);
+        this.triangleRight.setScale(1, this.proportion * 2);
+        this.triangleRight.setStrokeStyle(0.35, CONSTANTS.BASIC_COLORS.BLACK);
+        this.container.add(this.triangleRight);
+    }
+
+    createTrayRectangle() {
+        this.trayRectangle = this.scene.add.rectangle(this.x, this.y, this.width, this.height, this.color);
+        this.container.add(this.trayRectangle);
+        this.trayRectangle.setAlpha(this.transparentParameter);
+        this.trayRectangle.setStrokeStyle(1, CONSTANTS.BASIC_COLORS.BLACK);
     }
 
     setVisibility(isVisible) {
